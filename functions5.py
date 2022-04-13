72351be09a3ba464a7eb039f7561cb99d796697c
NaveenGunturu <naveen.gunturu0011@gmail.com>
2022-04-13 12:54:05 +0530

    function-commit

--- functions5.py
+++ functions5.py
@@ -1,5 +1,8 @@
-def list1(a):
-  list1=[12,2,3,5]
-  for x in a:
+def evenFilter(lista):
+  listb=[]
+  for x in lista:
     if x%2==0:
-      return a
+      listb.append(x)
+  return listb
+listn=[3,356,34,5,344,131,352,353,353,5623,353,622]
+print(evenFilter(listn))
