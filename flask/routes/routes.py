25439c01b9c063b832d6ce96841a78eb77998f3f
NaveenGunturu <naveen.gunturu0011@gmail.com>
2022-04-19 14:43:09 +0530

    python-commit

---
+++ flask/routes/routes.py
@@ -0,0 +1,9 @@
+from config import app
+from flask import jsonify
+from models import people
+
+@app.route("/people")
+def getPeople():
+    listp=people.getPeople()
+    result = [x.serialize() for x in listp]
+    return jsonify(result)
